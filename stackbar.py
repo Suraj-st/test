7597653489c31ccc9d6cf42c8c45f222dccac1dd
Suraj Tharaka <72661670+Suraj-st@users.noreply.github.com>
2022-05-16 05:04:25 +0530

    Create stackbar.py

---
+++ stackbar.py
@@ -0,0 +1,14 @@
+def stackedbar(data, predictor, target):
+    count = data[predictor].nunique()
+    sorter = data[target].value_counts().index[-1]
+    tabl = pd.crosstab(data[predictor], data[target], margins=True).sort_values(by=sorter, ascending=False)
+    print(tabl)
+    print("*"*120)
+    tab = pd.crosstab(data[predictor], data[target], normalize='index').sort_values(by=sorter, ascending=False)
+    tab.plot(kind='bar', stacked=True, figsize=(count+1, 5))
+    plt.legend(
+    loc="lower left",
+    frameon=False,
+    )
+    plt.legend(loc='upper left', bbox_to_anchor=(1,1))
+    plt.show()
